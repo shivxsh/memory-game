0d9818cf5308f12fcbe15fc8608616c19ec9c137
shivxsh <sreeshiveshpsg@gmail.com>
2024-06-06 08:54:20 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <div className='bg-sky-950 grid justify-center p-10 w-screen h-screen font-bold text-5xl font-Honk gap-6'>
+      Memory Match
+
+    </div>
+  )
+}
+
+export default App
