056b1a9630516af222eb69d0dccccbe9fe2911f3
shivxsh <sreeshiveshpsg@gmail.com>
2024-06-06 17:53:39 +0530

    🧹

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import GridCards from "./components/GridCards"
 
 function App() {
   return (
-    <div className="justify-center p-5 w-screen h-screen text-center">
-
-      <h1 className="font-bold text-6xl tracking-wide font-Honk mb-2">
+    <div className="justify-center p-4 w-screen h-screen text-center">
+      <h1 className="font-bold text-5xl md:text-7xl tracking-wide font-Honk mb-4">
         Magic Match
       </h1>
 
       <GridCards />
-
     </div>
   )
 }
